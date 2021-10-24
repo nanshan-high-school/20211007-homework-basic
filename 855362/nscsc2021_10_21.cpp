9bea977267d6b166cf2fd7a72f35cba1f670a6b2
Wififul <fishdidi7272@gmai.com>
2021-10-24 18:21:19 +0800

    新增855362作業#2

---
+++ 855362/nscsc2021_10_21.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout <<"Plase enter a number:";  
+  
+  int num;
+  cin >> num;  
+  
+  if (num%2==0){
+    cout <<num<<" is a evan number";
+    }else{
+    cout<<num<<" is a odd number";
+    }
+}
