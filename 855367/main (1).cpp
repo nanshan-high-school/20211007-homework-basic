3240fb6e71f520b9c44a0bfe9dfe74a119887c17
Barry20070221 <a855367@stdmail.nssh.ntpc.edu.tw>
2021-10-24 16:22:18 +0800

    新增855367作業1,2

---
+++ 855367/main (1).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int num;
+  cout <<"請輸入一個整數:";
+  cin >> num;
+  if(num%2==0){
+  cout << num <<"為偶數";
+ }
+  else{
+  cout << num <<"為奇數";
+ }
+}
