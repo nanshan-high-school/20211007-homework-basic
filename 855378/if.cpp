85d6e8d80e614097142b0b0d7830c9e28d343edf
Hank <hanklin5233@gmail.com>
2021-10-24 16:22:43 +0800

    新增855378作業#1

---
+++ 855378/if.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int num;
+  cout << "輸入一個整數:";
+  cin >> num;
+  if (num % 2 == 0){
+    cout << "偶數";
+  }else{
+    cout<< "奇數";
+  }
+
+  
+}
+
+  
